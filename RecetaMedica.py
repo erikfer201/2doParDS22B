d96f7be01360bcfbf53b6b313545fdf409354bed
erikfer201 <fernandoestradacruz41@gmail.com>
2022-12-05 21:32:44 -0600

    Examen segundo parcial

---
+++ RecetaMedica.py
@@ -0,0 +1,21 @@
+
+##Receta medica para el paciente 
+
+#Mensaje de bienvenida
+print ("Bienvenido al consultorio Estrada")
+
+#Agregamos el nombre del medico y del paciente
+nombreMedico = input ('Ingresa el nombre del medico: ')
+nombrePaciente = input ('Ingresa el nombre del paciente: ')
+#Agregamos los sintomas que padece
+sintomasPaciente = input ('Sintomas del paciente: ')
+#Ingresamos la dosis de medicamento que consumira asi como el total de dias 
+dosisMedicamento = input ('Ingresa el valor de la dosis: ')
+totalMedicamento = input ('Ingresa el total de dias a consumir: ')
+
+#Imprimimos los resultados
+print ('Nombre del Medico: ' +nombreMedico )
+print ('Nombre del paciente: ' +nombrePaciente )
+print ('Sintomas: ' +sintomasPaciente)
+print ('Dosis a consumir: ' +dosisMedicamento)
+print("Firma del medico: " +nombreMedico+ "Especialista en enfermedades")
